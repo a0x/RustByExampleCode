56802da5ff3dd261fd250b2738c2b74a345dcfbc
Alvin Qi <Alvin Qi@DESKTOP-TF>
2016-04-17 00:10:31 +0800

    add comments

--- RustTuror/src/main.rs
+++ RustTuror/src/main.rs
@@ -111,12 +111,21 @@ fn testcase() {
 
     impl fmt::Display for List {
         fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+            // Dereference `self` and create a reference to `vec`
+            // via destructuring.
             let List(ref vec) = *self;
             try!(write!(f, "["));
+
+            // Iterate over `vec` in `v` while enumerating the iteration
+            // count in `count`.
             for (count, v) in vec.iter().enumerate() {
+                // For every element except the first, add a comma
+                // before calling `write!`. Use `try!` to return on errors.
                 if count != 0 { try!(write!(f, ", ")); }
                 try!(write!(f, "{}", v));
             }
+
+            // Close the opened bracket and return a fmt::Result value
             write!(f, "]")
         }
     }
