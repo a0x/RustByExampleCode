f7d01c4b2a95d92069ea44f38bbac94ae060a17d
Alvin Qi <Alvin Qi@DESKTOP-TF>
2016-04-19 14:30:41 +0800

    add literals_and_operators func

--- RustTuror/src/main.rs
+++ RustTuror/src/main.rs
@@ -16,6 +16,7 @@ fn main() {
   display();
   testcase();
   formatting();
+  literals_and_operators();
 }
 
 fn formatted_print() {
@@ -181,4 +182,18 @@ fn formatting() {
     ].iter() {
         println!("{}", *color)
     }
+}
+
+fn literals_and_operators() {
+    println!("1 + 2 = {}", 1u32 + 2);
+    println!("1 - 2 = {}", 1i32 - 2);
+    println!("true AND false is {}", true && false);
+    println!("true OR  false is {}", true || false);
+    println!("NOT true is {}", !true);
+    println!("0011 AND 0101 is {:04b}", 0b0011u32 & 0b0101);
+    println!("0011 OR  0101 is {:04b}", 0b0011u32 | 0b0101);
+    println!("0011 XOR 0101 is {:04b}", 0b0011u32 ^ 0b0101);
+    println!("1 << 5 is {}", 1u32 << 5);
+    println!("0x80 >> 2 is 0x{:x}", 0x80u32 >> 2);
+    println!("ONe million is written as {}", 1_000_000u32);
 }
