78b97c3306bd8fd5fca202f083832e5875e8e79f
Alvin Qi <Alvin Qi@DESKTOP-TF>
2016-04-19 16:55:37 +0800

    add arrays_and_slices func

--- RustTuror/src/main.rs
+++ RustTuror/src/main.rs
@@ -3,6 +3,7 @@
  */
 
 use std::fmt::{self, Formatter, Display};
+use std::mem;
 
 fn main() {
   println!("Hello World!");
@@ -18,6 +19,7 @@ fn main() {
   formatting();
   literals_and_operators();
   tuples();
+  arrays_and_slices();
 }
 
 fn formatted_print() {
@@ -244,4 +246,30 @@ fn tuples() {
     println!("{:?}", matrix);
     println!("Matrix: \n{}", matrix);
     println!("Transpose: \n{}", transpose(matrix));
+}
+
+fn arrays_and_slices() {
+    fn analyze_slice(slice: &[i32]) {
+        println!("first element of the slice: {}", slice[0]);
+        println!("the slice has {} elements", slice.len());
+    }
+
+    #[derive(Debug)]
+    [i32; 500];
+
+    let xs: [i32; 5] = [1, 2, 3, 4, 5];
+    let ys: [i32; 500] = [0; 500];
+
+    println!("first element of the array: {}", xs[0]);
+    println!("second element of the array: {}", xs[1]);
+    println!("array size: {}", xs.len());
+    println!("array `xs` occupies {} bytes", mem::size_of_val(&xs));
+    println!("array `ys` occupies {} bytes", mem::size_of_val(&ys));
+
+    println!("borrow the whole array as a slice");
+    analyze_slice(&xs);
+
+    println!("borrow a section of the array as a slice");
+    analyze_slice(&ys[1..4]);
+    println!("{}", xs[5]);
 }
