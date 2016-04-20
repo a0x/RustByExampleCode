baf0808c53dbe7d89077ea17f35ac204ad869dbb
Alvin Qi <peng.edy@gmail.com>
2016-04-20 19:38:20 +0800

    restyle the code

--- main.rs
+++ main.rs
@@ -7,55 +7,55 @@ use std::fmt::{self, Formatter, Display};
 use std::mem;
 
 fn main() {
-  println!("Hello World!");
-  println!("I'm a Rustacean!");
+    println!("Hello World!");
+    println!("I'm a Rustacean!");
 
-  let x = 5 + 5;
-  println!("Is `x` 10 or 100? x = {}", x);
+    let x = 5 + 5;
+    println!("Is `x` 10 or 100? x = {}", x);
 
-  formatted_print();
-  debug();
-  display();
-  testcase();
-  formatting();
-  literals_and_operators();
-  tuples();
-  arrays_and_slices();
-  structures();
-  enumeration();
-  c_like();
-  linked_list();
+    formatted_print();
+    debug();
+    display();
+    testcase();
+    formatting();
+    literals_and_operators();
+    tuples();
+    arrays_and_slices();
+    structures();
+    enumeration();
+    c_like();
+    linked_list();
 }
 
 fn formatted_print() {
-  println!("{} days", 31);
-  println!("{0}, this is {1}. {1}, this is {0}", "Alice", "Bob");
-  println!("{subject} {verb} {object}",
-           subject = "The quick brown fox",
-           verb = "jumps over",
-           object = "the lazy dog.");
-  println!("{} of {:b} people know binary, the other half don't.", 1, 2);
-  println!("{number:>width$}", number=1, width=6);
-  println!("{number:>0width$}", number=1,width=6);
-  println!("My name is {0}, {1} {0}.", "Bond", "James");
+    println!("{} days", 31);
+    println!("{0}, this is {1}. {1}, this is {0}", "Alice", "Bob");
+    println!("{subject} {verb} {object}",
+             subject = "The quick brown fox",
+             verb = "jumps over",
+             object = "the lazy dog.");
+    println!("{} of {:b} people know binary, the other half don't.", 1, 2);
+    println!("{number:>width$}", number=1, width=6);
+    println!("{number:>0width$}", number=1,width=6);
+    println!("My name is {0}, {1} {0}.", "Bond", "James");
 
-  println!("Pi is roughly {0:.3}", 22.0/7.0);
-  println!("Pi is roughly {1:.0$}", 3, 22.0/7.0);
-  println!("Pi is roughly {:.*}", 3, 22.0/7.0);
-  println!("Pi is roughly {1:.*}", 3, 22.0/7.0);
+    println!("Pi is roughly {0:.3}", 22.0/7.0);
+    println!("Pi is roughly {1:.0$}", 3, 22.0/7.0);
+    println!("Pi is roughly {:.*}", 3, 22.0/7.0);
+    println!("Pi is roughly {1:.*}", 3, 22.0/7.0);
 }
 
 fn debug() {
-  #[derive(Debug)]
-  struct Structure(i32);
+    #[derive(Debug)]
+    struct Structure(i32);
 
-  #[derive(Debug)]
-  struct Deep(Structure);
+    #[derive(Debug)]
+    struct Deep(Structure);
 
-  println!("{:?} months in a year.", 12);
-  println!("{1:?} {0:?} is the {actor:?} name.", "Slater", "Christina", actor="actor's");
-  println!("Now {:?} will print!", Structure(3));
-  println!("Now {:?} will print!", Deep(Structure(7)));
+    println!("{:?} months in a year.", 12);
+    println!("{1:?} {0:?} is the {actor:?} name.", "Slater", "Christina", actor="actor's");
+    println!("Now {:?} will print!", Structure(3));
+    println!("Now {:?} will print!", Deep(Structure(7)));
 }
 
 fn display() {
@@ -170,7 +170,7 @@ fn formatting() {
     impl Display for Color {
         fn fmt(&self, f: &mut Formatter) -> fmt::Result {
             write!(f, "RGB ({}, {}, {}) 0x{:02X}{:02X}{:02X}",
-                   self.red, self.green, self.blue, self.red, self.green, self.blue)
+            self.red, self.green, self.blue, self.red, self.green, self.blue)
         }
     }
 
@@ -219,7 +219,7 @@ fn tuples() {
     impl Display for Matrix {
         fn fmt(&self, f: &mut Formatter) -> fmt::Result {
             write!(f, "( {} {} )\n( {} {} )",
-                   self.0, self.1, self.2, self.3)
+            self.0, self.1, self.2, self.3)
         }
     }
 
