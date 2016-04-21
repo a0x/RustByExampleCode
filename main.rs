d680c5d6e4a2de934fb174175a0a3dd1903fa9e4
Alvin Qi <peng.edy@gmail.com>
2016-04-21 16:01:21 +0800

    add expressions example

--- main.rs
+++ main.rs
@@ -33,6 +33,7 @@ fn main() {
     literals();
     interfaces();
     alias();
+    expressions();
 }
 
 fn formatted_print() {
@@ -545,4 +546,20 @@ fn alias() {
              nanoseconds,
              inches,
              nanoseconds + inches);
+}
+
+fn expressions() {
+    let x = 5u32;
+    let y = {
+        let x_squared = x * x;
+        let x_cube = x_squared * x;
+        x_cube + x_squared + x
+    };
+    let z = {
+        2 * x;
+    };
+
+    println!("x is {:?}", x);
+    println!("y is {:?}", y);
+    println!("z is {:?}", z);
 }
